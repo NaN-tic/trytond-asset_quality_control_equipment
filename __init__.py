0532c99fdc935a6b4429d1ffca8e127fc8e81996
Albert Cervera i Areny <albert@nan-tic.com>
2018-11-02 19:22:00 +0100

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .asset import *
-from .quality_control import *
+from . import asset
+from . import quality_control
 
 
 def register():
     Pool.register(
-        Asset,
-        AssetProofMethod,
-        ProofMethod,
-        EquipmentTemplate,
-        Template,
-        EquipmentTest,
-        Test,
+        asset.Asset,
+        asset.AssetProofMethod,
+        quality_control.ProofMethod,
+        quality_control.EquipmentTemplate,
+        quality_control.Template,
+        quality_control.EquipmentTest,
+        quality_control.Test,
         module='asset_quality_control_equipment', type_='model')
